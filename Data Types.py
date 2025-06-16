925ca0f537b4c1291caa22f638f8c4493119a0cf
VictoryNgige <victoryngige17@gmail>
2025-06-16 11:38:21 +0300

    initial commit

---
+++ Data Types.py
@@ -0,0 +1,47 @@
+number = 45 #Integer
+second = 34.98 #float
+greeting = "Good Morning" #String
+ispythoninteresting = True #Boolean
+
+#Data Structures
+fruits = ["apple", "banana", "cherry"]  #List
+cars = ("Jeep", "BMW", "Tesla", "Toyota") #Tuple
+countries = {"Kenya", "Jamaica", "Bahamas"} #Set
+student = {
+    "firstname" : "Victory",
+    "lastname" : "Ngige",
+    "age" : 18,
+    "nationality" : "Ethopian",
+} #Dictionary - Key-value pair
+
+print(number)
+print(ispythoninteresting)
+print(fruits)
+print(cars)
+print(countries)
+print(student)
+print(student["nationality"])
+
+
+#Typecasting - Converting one datatype to another
+print(float(number))
+print(int(second))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Variables.py
@@ -0,0 +1,25 @@
+school = "eMobilis"
+firstname = "Victory"
+lastname = "Ngige"
+num1 = 17
+num2 = 700
+language = "Python"
+
+print(school)
+print(firstname,lastname)
+print("My full name is", firstname, lastname)
+print(language,"is a web programming language.")
+print("The sum of", num1, "and", num2, "is", num1 + num2)
+print("My full name is", firstname, lastname, "and I study at", school)
+
+
+
+
+
+
+
+
+
+
+
+
